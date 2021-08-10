7c73a4bff96fcad77ed0a28795f75d1e30392015
Murat Kardeşler <murat.kardesler3019@gmail.com>
2021-08-10 23:54:31 +0300

    Sayfa Düzenleme işlemi
    Sertifika hobi ve iletişim sayfaları getirilme işlemi yapıldı

--- Cv_Asp.net/Controllers/HakkımdaController.cs
+++ Cv_Asp.net/Controllers/HakkımdaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Cv_Asp.net.Models.Entity;
+using Cv_Asp.net.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,27 @@ namespace Cv_Asp.net.Controllers
     public class HakkımdaController : Controller
     {
         // GET: Hakkımda
+        DbCvEntities db = new DbCvEntities();
+        GenericRepository<Tbl_Hakkımda> repo = new GenericRepository<Tbl_Hakkımda>();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var hakkimda = repo.List();
+
+            return View(hakkimda);
+        }
+        public ActionResult Index(Tbl_Hakkımda p)
+        {
+            var t = repo.Find(x => x.ID == 1);
+            t.Ad = p.Ad;
+            t.SoyAd = p.SoyAd;
+            t.Adres = p.Adres;
+            t.Mail = p.Mail;
+            t.Telefon = p.Telefon;
+            t.Acıklama = p.Acıklama;
+            t.Resim = p.Resim;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Cv_Asp.net/Controllers/HobiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cv_Asp.net.Models.Entity;
+using Cv_Asp.net.Repositories;
+
+namespace Cv_Asp.net.Controllers
+{
+    public class HobiController : Controller
+    {
+        // GET: Hobi
+        GenericRepository<Tbl_Hobiler> repo = new GenericRepository<Tbl_Hobiler>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            
+            var hobiler = repo.List();
+            return View(hobiler);
+        }
+        //güncelleme işlemi için
+        [HttpPost]
+        public ActionResult Index(Tbl_Hobiler p)
+        {
+            var t = repo.Find(x => x.ID == 1);
+            t.Acıklama1 = p.Acıklama1;
+            t.Acıklama2 = p.Acıklama2;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
